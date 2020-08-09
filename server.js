6a6c3a886b0a6a93a424bee0834504e4bb2377ba
ederrosa <eder-x@hotmail.com>
2020-08-09 17:46:53 -0300

    publicacao com correcoes v8

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 8082;
 
-app.use(express.static.apply(_dirname + '/dist/villsec'));
+app.use(express.static(_dirname + '/dist/villsec'));
 
 app.get('/*', (request, response) => {
-    response.sendFile(_dirname + '/dist/v1llse/index.html');
+    response.sendFile(path.join(_dirname + '/dist/villsec/index.html'));
 });
 
 app.listen(PORT, () => {
