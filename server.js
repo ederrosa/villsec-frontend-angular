bfeeacacceb39147aa6cd8dec785ef70f1fe2c84
ederrosa <eder-x@hotmail.com>
2020-08-09 18:17:19 -0300

    correcoes prod v9

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 8082;
 
-app.use(express.static(_dirname + '/dist/villsec'));
+app.use(express.static(__dirname + '/dist/villsec'));
 
 app.get('/*', (request, response) => {
-    response.sendFile(path.join(_dirname + '/dist/villsec/index.html'));
+    response.sendFile(path.join(__dirname + '/dist/villsec/index.html'));
 });
 
 app.listen(PORT, () => {
